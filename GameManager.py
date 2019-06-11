45a8a6b362d9a03aeac91af1c47c26fc19ffd329
Josh Bright <josh.bright@gmail.com>
2019-06-10 22:36:33 -0700

    Have a basic window with a red rect

---
+++ GameManager.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+class GameManager:
+    def __init__(self):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def draw(self, canvas):
+        pygame.draw.rect(canvas, (255, 0, 0), (25, 25, 25, 25))
+
+    def handle_event(self, event):
+        pass
---
+++ main.py
@@ -0,0 +1,7 @@
+from Engine.Engine import Engine
+from GameManager import GameManager
+
+
+if __name__ == '__main__':
+    e = Engine(GameManager)
+    e.game_loop()
