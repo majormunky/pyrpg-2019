b998d1a59fad6edf89e3f6035e6cc1dca41403ce
Josh Bright <josh.bright@gmail.com>
2020-01-27 17:08:32 -0800

    Updated formatting

--- GameObjects/World.py
+++ GameObjects/World.py
@@ -8,9 +8,9 @@ class World:
         self.old_map = None
         self.current_map = None
         self.current_map_name = None
-        self.grid = None # Grid(data["grid"])
-        self.width = None # self.grid.width
-        self.height = None # self.grid.height
+        self.grid = None
+        self.width = None
+        self.height = None
         self.size = 32
         self.image = None
         self.rect = None
@@ -26,7 +26,9 @@ class World:
             self.render_image()
 
     def render_image(self):
-        self.image = pygame.Surface((self.width * self.size, self.height * self.size), pygame.SRCALPHA)
+        self.image = pygame.Surface(
+            (self.width * self.size, self.height * self.size), pygame.SRCALPHA
+        )
         self.rect = self.image.get_rect()
         for y in range(self.grid.height):
             for x in range(self.grid.width):
@@ -34,9 +36,9 @@ class World:
                 tile_data = self.get_tile_data(tile_id)
                 if tile_data:
                     pygame.draw.rect(
-                        self.image, 
-                        tile_data["color"], 
-                        (x * self.size, y * self.size, self.size, self.size)
+                        self.image,
+                        tile_data["color"],
+                        (x * self.size, y * self.size, self.size, self.size),
                     )
 
     def update(self, dt):
@@ -60,9 +62,9 @@ class World:
         tile_id = self.grid.get_cell(tx, ty)
         return {
             "tile_index": (tx, ty),
-            "tile_id": tile_id, 
+            "tile_id": tile_id,
             "rect": pygame.Rect(tx * self.size, ty * self.size, self.size, self.size),
-            "tile_data": self.get_tile_data(tile_id)
+            "tile_data": self.get_tile_data(tile_id),
         }
 
     def get_tiles(self, rect):
@@ -80,7 +82,7 @@ class World:
         if t3["rect"] not in cache:
             cache.append(t3["rect"])
             result.append(t3)
-        if t4["rect"] not in cache:    
+        if t4["rect"] not in cache:
             cache.append(t4["rect"])
             result.append(t4)
         return result
